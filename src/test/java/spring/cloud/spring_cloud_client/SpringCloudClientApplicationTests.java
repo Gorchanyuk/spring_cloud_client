51040effc124d5cbb6df355b2aecad4ce40caea6
Gorchanyuk <gorchanyk@gmail.com>
2022-01-13 01:14:37 +0400

    Инициализировал. Gроект клиента для Spring Cloud

---
+++ src/test/java/spring/cloud/spring_cloud_client/SpringCloudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.spring_cloud_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
